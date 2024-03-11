446683d6a957916004c0bf6f88e0b33db59e6e38
idokolenny <idokoleonardz@gmail.com>
2024-03-11 15:53:12 +0100

    feat: ui for the game
    i coded a ui for the rock paper scissor game

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 //initialize all the variables
 
 let buttons=document.querySelectorAll('button')
-let reset=document.querySelector('.reset')
+// let reset=document.querySelector('.reset')
 let container=document.querySelector('.container')
 let final=document.querySelector('.final-result')
 
@@ -18,44 +18,70 @@ function getNum(){
 let playerCount=0
 let computerCount=0
 let roundCount=1
+let playerScore=document.querySelector('.playerScore')
+let computerScore=document.querySelector('.computerScore')
 
 buttons.forEach(function(button) {
     button.addEventListener('click', () => {
         let playerChoice=button.textContent
-        
+        console.log(playerChoice)
+        if (playerChoice == "Rock" || playerChoice == "Paper" || playerChoice == "Scissors") {
             final.textContent=" "
-            if (playerCount !== 8 && computerCount !== 8){
-                finalWinner=playRound(playerChoice,getComputerChoice())
+            if (playerCount != 5 && computerCount != 5){
+                let compChoice=getComputerChoice()
+                finalWinner=playRound(playerChoice,compChoice)
                 if (finalWinner=="p"){
                     let rounds=document.createElement('p')
                     playerCount++
-                    rounds.textContent=`**Round ${roundCount}** Player: ${playerCount} Computer: ${computerCount} ; Player Won`
+                    playerScore.textContent=`${playerCount}`
+                    computerScore.textContent=`${computerCount}`
+                    rounds.textContent=`**Round ${roundCount}** computer choose ${compChoice} ; Player Won`
+                    rounds.style.color='green'
                     container.appendChild(rounds)
                 }
                 else if (finalWinner=="c"){
                     let rounds=document.createElement('p')
                     computerCount++
-                    rounds.textContent=`**Round ${roundCount}** Player: ${playerCount} Computer: ${computerCount} ; Computer Won`
+                    playerScore.textContent=`${playerCount}`
+                    computerScore.textContent=`${computerCount}`
+                    rounds.textContent=`**Round ${roundCount}** Computer choose ${compChoice} ; Computer Won`
+                    rounds.style.color='red'
                     container.appendChild(rounds)
                 } else{
                     let rounds=document.createElement('p')
-                    rounds.textContent=`**Round ${roundCount}** Player: ${playerCount} Computer: ${computerCount} ; Draw`
+                    playerScore.textContent=`${playerCount}`
+                    computerScore.textContent=`${computerCount}`
+                    rounds.textContent=`**Round ${roundCount}** Computer choose ${compChoice} ; Draw`
                     container.appendChild(rounds)
                 }
                 roundCount++
             }
-            while (playerCount == 5 || computerCount == 5) {
+            if (playerCount == 5 || computerCount == 5) {
                 if (playerCount > computerCount){
                     final.textContent="Congratulations you Won!..."
+                    final.style.color="green"
                 }
                 else if (playerCount < computerCount){
                     final.textContent="Ah! Sorry you lose..."
+                    final.style.color="red"
                 }
                 else{
                     final.textContent="its a tie"
                 }
             }
-            
+        }
+        else {
+            let rounds=document.querySelectorAll('.container p')
+            rounds.forEach((round)=> {
+                round.textContent=" "
+                playerCount=0
+                computerCount=0
+                roundCount=1
+                final.textContent=" "
+                playerScore.textContent=`${0}`
+                computerScore.textContent=`${0}`
+            })
+        }    
             })
         })
 
