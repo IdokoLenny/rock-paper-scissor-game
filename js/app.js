f6ede2aeefa7fe392e7870f0d9a08c4ff2aa31be
idokolenny <idokoleonardz@gmail.com>
2024-03-11 14:57:24 +0100

    feat: ui for the game
    i coded a ui for the rock paper scissor game

--- js/app.js
+++ js/app.js
@@ -1,6 +1,12 @@
+//initialize all the variables
+
+let buttons=document.querySelectorAll('button')
+let reset=document.querySelector('.reset')
+let container=document.querySelector('.container')
+let final=document.querySelector('.final-result')
 
 //  initialize the selection array 
-const gameArr=["rock","paper","scissor"]
+const gameArr=["Rock","Paper","Scissors"];
 
 // <!-- get a random number -->
 
@@ -8,18 +14,50 @@ function getNum(){
     return Math.floor(Math.random() * gameArr.length)
 }
 // <!-- getPlayerSelection -->
-
-function getPlayerSelection(){
-    let pChoice=prompt("Specify your choice: Rock or Paper or Scissor")
-    pChoice=pChoice.toLowerCase()
-    while (pChoice != "rock" && pChoice != "paper" && pChoice != "scissor") {
-        pChoice=prompt("please Specify the right choice: Rock or Paper or Scissor")
-        pChoice=pChoice.toLowerCase()
-    }
-    console.log(`you picked ${pChoice}`)
-    return pChoice;
-}
  
+let playerCount=0
+let computerCount=0
+let roundCount=1
+
+buttons.forEach(function(button) {
+    button.addEventListener('click', () => {
+        let playerChoice=button.textContent
+        
+            final.textContent=" "
+            if (playerCount !== 8 && computerCount !== 8){
+                finalWinner=playRound(playerChoice,getComputerChoice())
+                if (finalWinner=="p"){
+                    let rounds=document.createElement('p')
+                    playerCount++
+                    rounds.textContent=`**Round ${roundCount}** Player: ${playerCount} Computer: ${computerCount} ; Player Won`
+                    container.appendChild(rounds)
+                }
+                else if (finalWinner=="c"){
+                    let rounds=document.createElement('p')
+                    computerCount++
+                    rounds.textContent=`**Round ${roundCount}** Player: ${playerCount} Computer: ${computerCount} ; Computer Won`
+                    container.appendChild(rounds)
+                } else{
+                    let rounds=document.createElement('p')
+                    rounds.textContent=`**Round ${roundCount}** Player: ${playerCount} Computer: ${computerCount} ; Draw`
+                    container.appendChild(rounds)
+                }
+                roundCount++
+            }
+            while (playerCount == 5 || computerCount == 5) {
+                if (playerCount > computerCount){
+                    final.textContent="Congratulations you Won!..."
+                }
+                else if (playerCount < computerCount){
+                    final.textContent="Ah! Sorry you lose..."
+                }
+                else{
+                    final.textContent="its a tie"
+                }
+            }
+            
+            })
+        })
 
 // <!-- getComputerChoice -->
 
@@ -38,7 +76,7 @@ function playRound(playerSelection, computerChoice){
 
     if (playerSelection==computerChoice){
         winner="t"
-    }else if ((playerSelection=="rock" && computerChoice=="scissor") || (playerSelection=="scissor" && computerChoice=="paper") || (playerSelection=="paper" && computerChoice=="rock")){
+    }else if ((playerSelection=="Rock" && computerChoice=="Scissors") || (playerSelection=="Scissors" && computerChoice=="Paper") || (playerSelection=="Paper" && computerChoice=="Rock")){
         winner="p"
     }
     else {
@@ -47,39 +85,3 @@ function playRound(playerSelection, computerChoice){
     return winner
 }
 
-// <!-- write the playGame function to loop 5 times -->
-
-function playGame(){
-    let playerCount=0
-    let computerCount=0
-    let overAllWinner
-    for (let i=0; i<=4; i++){
-        finalWinner=playRound(getPlayerSelection(),getComputerChoice())
-        if (finalWinner=="p"){
-            playerCount++
-            console.log(`**Scores** \nPlayer: ${playerCount} \nComputer: ${computerCount}`)
-        }
-        else if (finalWinner=="c"){
-            computerCount++
-            console.log(`**Scores** \nPlayer: ${playerCount} \nComputer: ${computerCount}`)
-        } else{
-            console.log(`**Scores** \nPlayer: ${playerCount} \nComputer: ${computerCount}`)
-        }
-    }
-  
-    if (playerCount > computerCount){
-        overAllWinner="Congratulations you Won!..."
-        return overAllWinner
-    }
-    else if (playerCount < computerCount){
-        overAllWinner="Ah! Sorry you lose..."
-        return overAllWinner
-    }
-    else{
-        overAllWinner="its a tie"
-        return overAllWinner
-    }
-}
-
-let mainWinner=playGame()
-console.log(mainWinner)
